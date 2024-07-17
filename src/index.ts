423b73aeba5ee3787e705abc6746ad642d169e62
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-07-18 00:13:07 +0530

    docs: add txn samples and setup guide

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import { Hono } from "hono";
 
 const app = new Hono();
 
-app.post("/", async (c) => {
+app.post("/", async (c: any) => {
+  if (
+    !c.req.header("Authorization") ||
+    c.req.header("Authorization") !== c.env.AUTH_TOKEN
+  ) {
+    return c.json(
+      {
+        message: "Unauthorized",
+      },
+      401
+    );
+  }
+
   const body = await c.req.json();
   console.log("Data received by webhook", JSON.stringify(body, null, 2));
-  console.log("Authorization Header - ", c.req.header("Authorization"));
   console.log("Headers -", c.req.header());
   console.log("Query Params -", c.req.query("param"));
 
